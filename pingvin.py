19057623ef0c9636843edb4588af329107fe05c7
Dmitriy Ivanov <f1-ks@mail.ru>
2019-12-19 22:12:44 +0400

    2

---
+++ pingvin.py
@@ -0,0 +1,7 @@
+k = int(input("Ввидиде число: " ))
+
+print('   _~_    ' * k)
+print('  (o o)   ' * k)
+print(' /  V  \\  ' * k)
+print('/(  _  )\\ ' * k)
+print('  ^^ ^^   ' * k)
---
+++ start.py
@@ -0,0 +1,4 @@
+f = open ("pingvin.py", 'r')
+
+
+print (f)
